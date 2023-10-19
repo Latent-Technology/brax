2100f0fafd6bf0a847135b25c8bd0ce9831b2c53
Andy Aitken <andy@latent-technology.com>
2023-10-19 16:00:32 +0100

    change revsion tag

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup
 
 setup(
     name="latent-brax",
-    version="0.1.2",
+    version="0.1.1.1",
     description=("A differentiable physics engine written in JAX."),
     author="Brax Authors",
     author_email="no-reply@google.com",
